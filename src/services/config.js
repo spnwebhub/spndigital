4b485d7f9639b82766215cfa77f14562d1f0540a
SPN Web <pcalfa@spnweb.com.br>
2024-06-06 20:06:54 +0000

    feat: configure script to start tailwind and firebase authentication

---
+++ src/services/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8U28T56JGl3wWyj4zavVbPZojwDcSEB4",
+  authDomain: "leao-do-norte-auth.firebaseapp.com",
+  projectId: "leao-do-norte-auth",
+  storageBucket: "leao-do-norte-auth.appspot.com",
+  messagingSenderId: "299257617455",
+  appId: "1:299257617455:web:74b47a9786afefdb0d3ada"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
